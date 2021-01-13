923dda23db4821932c63eda5074af618e3471348
lenhat509 <lenguyencongnhat2001@gmail.com>
2021-01-13 14:40:35 -0800

    finish simple linear regression algo

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
-learning_rate = 0.1
+learning_rate = 0.00001
 data = pd.read_csv('data.csv', header=None)
 weights = np.array([0, 0])
 output = data[1]
@@ -13,10 +13,11 @@ input1 = np.full(100, 1)
 input[0] = input1
 input = input.join(data[[0]].rename(columns={0: 1}))
 
-next_weights = np.array([1, 1])
+partial_derivative = np.array([1, 1])
 
-while not (weights == next_weights).all():
-    weights = next_weights
-    partial_derivative = (input * ((input * weights).sum(axis=1) - output).values.reshape((100, 1))).sum(axis=0)
-    next_weights = weights - learning_rate * partial_derivative
+while not (partial_derivative == [0, 0]).all():
+    partial_derivative = (input * ((input * weights).sum(axis=1) - output).values.reshape((100, 1))).sum(axis=0).round(5)
+    weights = weights - learning_rate * partial_derivative
     print(partial_derivative)
+
+print(weights)
